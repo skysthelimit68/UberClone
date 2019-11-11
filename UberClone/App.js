303827502666c23a00fc6ec90219eed668758cb0
Ming Liu <skysthelimit68@gmail.com>
2019-11-11 01:05:34 -0500

    recenter map component

--- UberClone/App.js
+++ UberClone/App.js
@@ -4,6 +4,7 @@ import MapView from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 import { DestinationButton } from "./components/DestinationButton.js";
+import { CurrentLocationButton } from "./components/CurrentLocationButton.js";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,28 +20,35 @@ export default class App extends React.Component {
     if (status !== "granted") {
       console.log("Permission not granted");
     }
-    let location = await Location.getCurrentPositionAsync({});
+    let location = await Location.getCurrentPositionAsync({enabledHighAccuracy: true});
     let region = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
       latitudeDelta: 0.045,
-      longitudeDelta: 0.045
+      longitudeDelta: 0.045,
     };
     this.setState({
       region: region
     });
   };
 
+  centerMap() {
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = this.state.region;
+    this.map.animateToRegion({ latitude, longitude, latitudeDelta, longitudeDelta })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <DestinationButton />
+        <CurrentLocationButton cb = {() => {this.centerMap() }}/>
         <MapView
           initialRegion={this.state.region}
           showsUserLocation={true}
           showsCompass={true}
           rotateEnabled={false}
-          style={{ flex: 1 }}
+          ref={(map) => {this.map = map}}
+          style={{ flex: 1 , zIndex: 0}}
         />
       </View>
     );
